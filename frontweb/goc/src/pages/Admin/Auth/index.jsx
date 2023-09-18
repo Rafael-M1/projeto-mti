ae0b0dc7f5bcd44d8432a8ac12ead4743e481877
rafael.silva <rafaelsilva@seplag.mt.gov.br>
2023-09-18 10:28:29 -0400

    Image Pagina de Login

--- frontweb/goc/src/pages/Admin/Auth/index.jsx
+++ frontweb/goc/src/pages/Admin/Auth/index.jsx
@@ -1,5 +1,6 @@
 // import { ReactComponent as LoginImage } from "./../../../assets/images/login-image.png";
 import loginImage from "./../../../assets/images/login-image.png";
+import loginImage2 from "./../../../assets/images/login-logo2.jpg";
 import { Link } from "react-router-dom";
 import Login from "./Login";
 
@@ -11,7 +12,7 @@ const Auth = ({ toggleAuth }) => {
     <div className="auth-container">
       <div className="auth-banner-container">
         <h1>Gestão de Ocorrências Criminais</h1>
-        {/* <img className="login-image" src={loginImage}></img> */}
+        <img className="login-image" src={loginImage2}></img>
       </div>
       <div className="auth-form-container">
         <Login toggleAuth={toggleAuth} />
