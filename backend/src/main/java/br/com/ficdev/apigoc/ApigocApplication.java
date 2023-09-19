5991b21b38ca0b6e318588b6d80d5e0c099f0cf0
rafael.silva <rafaelsilva@seplag.mt.gov.br>
2023-09-19 11:27:18 -0400

    Alterações projeto para deploy no Tomcot

--- backend/src/main/java/br/com/ficdev/apigoc/ApigocApplication.java
+++ backend/src/main/java/br/com/ficdev/apigoc/ApigocApplication.java
@@ -2,9 +2,10 @@ package br.com.ficdev.apigoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApigocApplication {
+public class ApigocApplication extends SpringBootServletInitializer  {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApigocApplication.class, args);
--- frontweb/goc/src/main.jsx
+++ frontweb/goc/src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import {registerLocale} from "react-datepicker";
+import { registerLocale } from "react-datepicker";
 import ptBr from "date-fns/locale/pt-BR";
 // import './index.css';
-registerLocale('pt-BR', ptBr);
+registerLocale("pt-BR", ptBr);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/helloworld/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
