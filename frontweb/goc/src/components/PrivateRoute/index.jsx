fe3416205f72b4d6835158d378852935e24757fd
rafael.silva <rafaelsilva@seplag.mt.gov.br>
2023-09-21 11:40:21 -0400

    Private Route

--- frontweb/goc/src/components/PrivateRoute/index.jsx
+++ frontweb/goc/src/components/PrivateRoute/index.jsx
@@ -1,37 +1,12 @@
-/*import { Route, Navigate } from 'react-router-dom';
-import { hasAnyRoles, isAuthenticated } from './../../util/auth';
+import { Navigate, Outlet } from "react-router-dom";
 
-const PrivateRoute = ({ children, path, roles = [] }) => {
-  return (
-    <Route
-      path={path}
-      render={({ location }) =>
-        !isAuthenticated() ? (
-          <Navigate to={{ pathname: '/admin/auth/login' }} />
-        ) : !hasAnyRoles(roles) ? (
-          <Navigate to={{ pathname: '/admin/products' }} />
-        ) : (
-          children
-        )
-      }
-    />
-  );
-};
-
-const ProtectedRoute = ({ component, requiredRoles }) => {
-  if (isAuthenticatedV2() && isAuthorizedV2(requiredRoles)) {
-    return component;
-  } else {
-    return <Navigate to="/" />;
+const PrivateRoute = ({
+  isAllowed,
+  redirectPath = "/apigoc/admin/auth",
+  children,
+}) => {
+  if (!isAllowed) {
+    return <Navigate to={redirectPath} replace />;
   }
+  return children ? children : <Outlet />;
 };
-
-const isAuthenticatedV2 = () => {
-  return true;
-};
-
-const isAuthorizedV2 = (requiredRoles) => {
-  return true;
-};
-
-export default PrivateRoute;*/
