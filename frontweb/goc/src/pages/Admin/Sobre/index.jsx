a072236d89065277cc592161abacab79927c07c5
rafael.silva <rafaelsilva@seplag.mt.gov.br>
2023-09-27 08:18:59 -0400

    Alteração pagina Sobre

--- frontweb/goc/src/pages/Admin/Sobre/index.jsx
+++ frontweb/goc/src/pages/Admin/Sobre/index.jsx
@@ -24,11 +24,31 @@ const SobrePage = () => {
       <div className="card-body">
         <h2 className="card-title text-center">Sobre</h2>
         <div className="container mt-5">
-          <h4>Tema do Projeto: Gestão de Ocorrências Criminais - FICDEV</h4>
-          <h5 className="mt-5">Autor: Rafael Muzzi da Silva</h5>
-          <h5 className="mt-3">Contatos:</h5>
-          <h5>E-mail:</h5> <p>rafael.muzzi2@gmail.com</p>
-          <h5>Github:</h5> <p>rafael-m1</p>
+          <div className="row">
+            <h4>Tema do Projeto: Gestão de Ocorrências Criminais - FICDEV</h4>
+            <h5 className="mt-4">
+              Descrição: Sistema para facilitar o acesso do cidadão ao serviço
+              de registro de ocorrência criminais.
+            </h5>
+            <h5 className="mt-3">Autor: Rafael Muzzi da Silva</h5>
+            <h5 className="mt-3">Contatos:</h5>
+            <div className="col-lg-4">
+              <h5>E-mail:</h5> <p>rafael.muzzi2@gmail.com</p>
+            </div>
+            <div className="col-lg-4">
+              <h5>Github:</h5> <p>rafael-m1</p>
+            </div>
+            <h5 className="mt-3">Tecnologias Usadas no Projeto:</h5>
+            <div className="col-lg-4">
+              <h5>Front-End:</h5> <p>React JS</p>
+            </div>
+            <div className="col-lg-4">
+              <h5>Back-end:</h5> <p>Java 17 com Framework Spring</p>
+            </div>
+            <div className="col-lg-4">
+              <h5>Banco de dados:</h5> <p>Postgresql 14</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
